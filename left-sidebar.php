8981bb2097b85304dd26a846f5e0bb4933cfdbeb
Rajeet212 <138962141+Rajeet212@users.noreply.github.com>
2023-11-15 19:55:25 +0600

    Add files via upload

---
+++ left-sidebar.php
@@ -0,0 +1,60 @@
+<div class="sidebar">
+      <div class="logo-details">
+        <i class="fas fa-book-open"></i>
+          <div class="logo_name">UIU DONATION CLUBS</div>
+      </div>
+      <ul class="nav-list">
+        <li>
+          <a href="cse.php" id="English">
+           <img src="img/con1.png"  width="50" height="50">
+            <span class="links_name">
+UIU Computer Club</span>
+          </a>
+        </li>
+        <li>
+         <a href="chemistry.php" id="Math">
+           <img src="img/con2.jpg" width="50" height="50" >
+           <span class="links_name">UIU Theatre & Flim Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="chemistry.php" id="Physics">
+          <img src="img/con3.jpg" width="50" height="50">
+           <span class="links_name">UIU Cultural Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="chemistry.php" id="Chemistry">
+           <img src="img/con4.jpg" width="50" height="50">
+           <span class="links_name">UIU APP FORUM</span>
+         </a>
+       </li>
+       <li>
+         <a href="cse.php" id="Economics">
+           <img src="img/con5.jpg" width="50" height="50">
+           <span class="links_name">UIU Photography Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="cse.php" id="Law">
+           <img src="img/con6.jpg" width="50" height="50">
+           <span class="links_name">UIU Sports Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="cse.php" id="cse">
+          <img src="img/con7.jpg" width="50" height="50">
+           <span class="links_name">UIU Robotics Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="physics.php" id="History">
+         <div class="logo-details">
+        <i class="fas fa-book-open"></i>
+          <div class="logo_name">UIU LOAN FORUM</div>
+      </div>
+         </a>
+       </li>
+      
+      </ul>
+    </div>
