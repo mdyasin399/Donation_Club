c19b0e20270dd884946d684ed51ddd9c4e82d094
yasin <mdyasin399.my@gmail.com>
2023-11-22 12:24:23 +0600

    about file added

---
+++ about.php
@@ -0,0 +1,168 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+div.gallery {
+  border: 1px solid #ccc;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.responsive {
+  padding: 0 6px;
+  float: left;
+  width: 25%;
+}
+
+@media only screen and (max-width: 700px) {
+  .responsive {
+    width: 49.99999%;
+    margin: 6px 0;
+  }
+}
+
+.about-section {
+  padding: 20px;
+  text-align: center;
+  background-color: #000000;
+  color: white;
+}
+
+@media only screen and (max-width: 500px) {
+  .responsive {
+    width: 100%;
+  }
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.clearfix:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<head>
+
+	<!---Title and title icon---->
+	<title>UIU Donation Club | About Us </title>
+	<link rel="shortcut icon" href="./uploads/d.png" type="img/x-icon" />
+	<!---Title and title icon end---->
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@600&family=Montserrat+Subrayada:wght@400;700&family=Secular+One&family=Source+Code+Pro:wght@700&family=Zen+Dots&display=swap" rel="stylesheet">
+ <!-- fonts ends -->
+
+
+    
+    <!-- css link starts  -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- css link starts  -->
+
+
+    <!-- iconscout cdn link starts  -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- iconscout cdn linkends  -->
+
+    <!-- image slider swiper js starts   -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    <!-- image slider swiper js ends -->
+
+ 
+
+  <div class="about-section">
+  <h1>Our Committee Members</h1>
+  <p>Our team is there to help bring members together for meets and events.</p>
+  <p>We host regular club meetings every Thursday evening, and at least one meeting per week.</p>
+</div>
+
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="responsive">
+  <div class="gallery">
+  <div class="card">
+    <a target="_blank" href="https://github.com/mdyasin399">
+      <img src="./img/yasin.jpg" alt="" width="600" height="400">
+    </a>
+    <div class="desc">Md Yasin</div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="responsive">
+  <div class="gallery">
+  <div class="card">
+    <a target="_blank" href="">
+      <img src="./img/rajeet.jpg" alt="" width="600" height="400">
+    </a>
+    <div class="desc">Rajeet</div>
+    </div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+  <div class="card">
+    <a target="_blank" href="">
+      <img src="./img/ayon.jpg" alt="" width="600" height="400">
+    </a>
+    <div class="desc">Ayon</div>
+    </div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+  <div class="card">
+    <a target="_blank" href="">
+      <img src="./img/labib.jpg" alt="" width="600" height="400">
+    </a>
+    <div class="desc">Labib</div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+   
+
+
+
