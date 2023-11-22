fd1ca59bd4b133812e19cbbac183c7fb9aa4332a
Md Yasin <83154902+mdyasin399@users.noreply.github.com>
2023-11-22 12:56:21 +0600

    left sidebar file added

--- left-sidebar.php
+++ left-sidebar.php
@@ -1,60 +1,59 @@
 <div class="sidebar">
       <div class="logo-details">
         <i class="fas fa-book-open"></i>
-          <div class="logo_name">UIU DONATION CLUBS</div>
+          <div class="logo_name">OUR CLUB PARTNERS</div>
       </div>
       <ul class="nav-list">
+
+      <li>
+         <a href="https://www.facebook.com/uiurobotics" id="cse">
+          <img src="img/con7.jpg" width="50" height="50">
+           <span class="links_name">&ensp;UIU Robotics Club</span>
+         </a>
+       </li>
+       <li>
+         <a href="https://www.facebook.com/UIUMRT" id="cse">
+          <img src="img/con9.jpg" width="50" height="50">
+           <span class="links_name">&ensp;UIU Mars Rover</span>
+         </a>
+       </li>
         <li>
-          <a href="cse.php" id="English">
+          <a href="https://www.facebook.com/uiuccl" id="English">
            <img src="img/con1.png"  width="50" height="50">
-            <span class="links_name">
-UIU Computer Club</span>
+            <span class="links_name">&ensp;UIU Computer Club</span>
           </a>
         </li>
         <li>
-         <a href="chemistry.php" id="Math">
+         <a href="https://www.facebook.com/uiutfclub" id="Math">
            <img src="img/con2.jpg" width="50" height="50" >
-           <span class="links_name">UIU Theatre & Flim Club</span>
+           <span class="links_name">&ensp;UIU Theatre & Flim Club</span>
          </a>
        </li>
        <li>
-         <a href="chemistry.php" id="Physics">
+         <a href="https://www.facebook.com/UIU.Cultural.club.uiucc" id="Physics">
           <img src="img/con3.jpg" width="50" height="50">
-           <span class="links_name">UIU Cultural Club</span>
+           <span class="links_name">&ensp;UIU Cultural Club</span>
          </a>
        </li>
        <li>
-         <a href="chemistry.php" id="Chemistry">
+         <a href="https://www.facebook.com/uiuappf" id="Chemistry">
            <img src="img/con4.jpg" width="50" height="50">
-           <span class="links_name">UIU APP FORUM</span>
+           <span class="links_name">&ensp;UIU APP FORUM</span>
          </a>
        </li>
        <li>
-         <a href="cse.php" id="Economics">
+         <a href="https://www.facebook.com/UIUPC" id="Economics">
            <img src="img/con5.jpg" width="50" height="50">
-           <span class="links_name">UIU Photography Club</span>
+           <span class="links_name">&ensp;UIU Photography Club</span>
          </a>
        </li>
        <li>
-         <a href="cse.php" id="Law">
+         <a href="https://www.facebook.com/SCUIU" id="Law">
            <img src="img/con6.jpg" width="50" height="50">
-           <span class="links_name">UIU Sports Club</span>
+           <span class="links_name">&ensp;UIU Sports Club</span>
          </a>
        </li>
-       <li>
-         <a href="cse.php" id="cse">
-          <img src="img/con7.jpg" width="50" height="50">
-           <span class="links_name">UIU Robotics Club</span>
-         </a>
-       </li>
-       <li>
-         <a href="physics.php" id="History">
-         <div class="logo-details">
-        <i class="fas fa-book-open"></i>
-          <div class="logo_name">UIU LOAN FORUM</div>
-      </div>
-         </a>
-       </li>
+ 
       
       </ul>
     </div>
