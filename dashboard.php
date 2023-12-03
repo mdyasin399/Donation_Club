a51f4814c59d210b66a2edb2da6f03b107b91fe8
yasin <mdyasin399.my@gmail.com>
2023-12-03 13:10:46 +0600

    dashboard update file added

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,43 @@
            <span class="hr-line"></span>
           <!--Loan post title after dubble line end -->
 
-     
+ <!--Loan post Details show -->
+   <?php 
+        $ques = mysqli_query($db,"SELECT q_id, Status, question_detail, img, ques_points, username, Subject_Name, created_at  FROM questions q
+       JOIN students s ON q.student_id=s.id
+       JOIN subjects sub ON q.subject_code=sub.s_code ORDER BY created_at DESC")or die(mysqli_error());
+      date_default_timezone_set("Asia/Dhaka");
+      while($q = mysqli_fetch_assoc($ques))
+          {
+            $now = new DateTime();
+            $p= new DateTime($q['created_at']);
+            $interval = $now->diff($p);
+
+            ?>
+      <div class="question" id = "response">
+        <div class="info">
+          <div class="info-title">
+            <img src="./uploads/<?php echo $q['img']; ?>"class="rounded-circle " height="35" width="35" alt=""
+              loading="lazy" style= "margin-right: 15px;"/>
+              <div style="text-transform: capitalize; margin-right: 20px;"><?php echo $q['username']; ?></div>
+              <div class="subject"> <p>Post valid Until <?php echo $q['Subject_Name']; ?> Months</p></div>
+              <div><?php echo ($interval->format("%a") * 24) + $interval->format("%h"). " hours". $interval->format(" %i minutes "); ?> ago</div>
+          </div>
+          <div class="point-bar"><i class="fas fa-star"></i><span class=""><?php echo $q['ques_points']; ?> points</div>
+        </div>
+        <div><a href="question.php?Question_ID=<?php echo $q['q_id']; ?>" class="ques_det"> <?php if(strlen($q['question_detail'])>150){ echo substr($q['question_detail'], 0, 150);} else { echo $q['question_detail'];}  ?> </a></div>
+          <div class="last-row"> 
+            <div class="status"><?php echo $q['Status']; ?></div>         
+            <div class="p-btn ">
+              <button type="button" <?php if($q['username']==$row['username'] || $q['Status']=="Donated"){?> disabled="disabled" style="display: none;" <?php } ?> name="Want to Donate" class="p-btn-des" style="border: 1px solid black;"><a href="secure-payment.php" style="text-decoration: none;color: black;">Donate</a></button></div>
+          </div>
+
+        <span class="hr-line"></span>
+      </div>
+   <?php 
+   }
+   ?>
+    <!--Loan post Details show end -->
 
     </div>
         
